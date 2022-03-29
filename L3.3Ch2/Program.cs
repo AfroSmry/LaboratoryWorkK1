99d990aa252e5fff5bb4a9d46cac3585a753f32b
AfroSmry <baranov.konstantin.d@gmail.com>
2022-03-29 15:21:09 +0300

    L3.3ch2 R

--- L3.3Ch2/Program.cs
+++ L3.3Ch2/Program.cs
@@ -2,11 +2,10 @@
 из чисел, которые встречаются в данном массиве более двух раз(если такие имеются). */
 
 Random random = new();
-//Console.WriteLine("Введите количество элементов одномерного массива.");
-int arrElements = 30;//Convert.ToInt32(Console.ReadLine());
+int arrElements = random.Next(10, 50);
 List<int> listStart = new(arrElements);
 List<int> listEnd = new();
-int counter = 0;
+int counter;
 Console.WriteLine("Значения внесенные в список.");
 
 // Заполнение списка.
@@ -18,42 +17,28 @@ for (int i = 0; i < arrElements; i++)
 //Поиск совпадений
 for (int i = 0; i < listStart.Count; i++)
 {
-    for (int j = i+1; j < listStart.Count; j++)
+    counter = 0;
+    for (int j = 0; j < listStart.Count; j++)
     {
         if (listStart[i] == listStart[j])
         {
-            counter++;
-            if (counter == 3)
-            {
-                listEnd.Add(listStart[i]);
-                counter = 0;
-            } 
-            
+            counter++;            
         }
 
     }
-}
-counter = 0;
-Console.WriteLine();
-Console.WriteLine("Сортированные повторяющиеся элементы до удаления.");
-foreach (var item in listEnd)
-{
-    Console.Write(item + " ");
-}
-
-for (int i = 0; i < listEnd.Count; i++)
-{
-    for (int j = 1; j < listEnd.Count; j++)
+    if (counter > 2)
     {
-        if (listEnd[i] == listEnd[j])
+        bool check = true;
+        for (int k = 0; check && k < listEnd.Count; k++)
         {
-            counter++;
-            if (counter==2 )
+            if (listEnd[k] == listStart[i])
             {
-                listEnd.Remove(listEnd[j]);
-                counter = 0;
-                j--;
-            }            
+                check = false;
+            }
+        }
+        if (check)
+        {
+            listEnd.Add(listStart[i]);
         }
     }
 }
