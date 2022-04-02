aa18d812f3e5791c207da9bd496c817ef5a7f742
AfroSmry <baranov.konstantin.d@gmail.com>
2022-04-02 20:42:35 +0300

    7.3

--- L7.3/Program.cs
+++ L7.3/Program.cs
@@ -1,3 +1,18 @@
 ﻿/* Дан текстовый файл, представляющий собой запись арифметического выражения,
 операндами которого являются однозначные числа. Число операций в выражении не больше
 двух. Вычислить значение этого выражения и дописать его в конец этого файла. */
+
+string path = @"D:\DotNET\laboratory work\LaboratoryWorkK1\L7.3\Ar.txt";
+string arifm = "2+2*2";
+string text;
+
+using (StreamWriter writer = new StreamWriter(path, false))
+{
+    await writer.WriteLineAsync(arifm);
+}
+
+using (StreamReader reader = new StreamReader(path))
+{
+    text = await reader.ReadToEndAsync();
+    Console.WriteLine(text);
+}
