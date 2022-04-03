5e552457791658656df73223c7b97736236dc7f3
AfroSmry <baranov.konstantin.d@gmail.com>
2022-04-03 18:53:14 +0300

    L8.3 готово

--- L7.3/Program.cs
+++ L7.3/Program.cs
@@ -5,14 +5,24 @@
 string path = @"D:\DotNET\laboratory work\LaboratoryWorkK1\L7.3\Ar.txt";
 string arifm = "2+2*2";
 string text;
+char[] chars = { '-', '+', '*' , '/' , ' '};
+List<int> dataInt = new();
+int result = 0;
 
-using (StreamWriter writer = new StreamWriter(path, false))
+using (StreamWriter writer = new(path, false))
 {
     await writer.WriteLineAsync(arifm);
 }
 
-using (StreamReader reader = new StreamReader(path))
+using (StreamReader reader = new(path))
 {
     text = await reader.ReadToEndAsync();
     Console.WriteLine(text);
+}
+
+var data = text.Split(chars, StringSplitOptions.None);
+
+for (int i = 0; i < data.Length; i++)
+{
+    dataInt.Add(int.Parse(data[i]));
 }
